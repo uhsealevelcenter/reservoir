c13d8a70c7a1bbf059c4fff546639bf7bcfdbf4b
Nemanja Komar <komar@hawaii.edu>
2020-04-16 12:45:23 -1000

    Load in geojson and display station locations on the map

--- js/mymap.js
+++ js/mymap.js
@@ -1,5 +1,5 @@
 var map = L.map('map',{
-}).setView([21.102, -157.327], 8);
+}).setView([21.102, -157.327], 7);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
   maxZoom: 18,
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
@@ -9,3 +9,31 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   tileSize: 512,
   zoomOffset: -1
 }).addTo(map);
+
+
+
+//Loading stations locations
+var mainGeoJSON = new L.GeoJSON.AJAX("stations.geojson");
+
+var stationPointsLayer = {};
+mainGeoJSON.on('data:loaded', function() {
+  console.log("Loaded", mainGeoJSON);
+  var geoJsonFormat = this.toGeoJSON();
+
+  stationPointsLayer = L.geoJSON(geoJsonFormat, {
+  // filter: function(feature, layer) {
+  //   return typeof feature.properties.sl_component !== "undefined";
+  // }
+});
+
+stationPointsLayer.addTo(map);
+
+});
+
+mainGeoJSON.on('data:progress', function() {
+  console.log("Progress");
+});
+
+mainGeoJSON.on('data:loading', function() {
+  console.log("Loading");
+});
